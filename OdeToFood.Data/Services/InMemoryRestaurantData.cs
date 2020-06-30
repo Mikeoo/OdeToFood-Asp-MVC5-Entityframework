fbe4d4a6b22b8a3e485d1ed4a20f9eab46956f65
Mike Overbeek <38927348+Mikeoo@users.noreply.github.com>
2020-07-01 01:42:37 +0200

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id = 1, Name = "Mike's Restaurant", Cuisine = CuisineType.French },
+                new Restaurant{Id = 2, Name = "Daves's Restaurant", Cuisine = CuisineType.Italian },
+                new Restaurant{Id = 3, Name = "Lionels's Restaurant", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
